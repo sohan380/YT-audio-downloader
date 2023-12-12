f9b069f749f53e645758a5a867af105cc47fe969
sohan380 <sohanmupparaju@gmail.com>
2023-12-12 22:23:26 +0530

    Added Instructions

--- YTD.py
+++ YTD.py
@@ -1,3 +1,5 @@
+# install pytube before running. Use - pip3 install pytube
+
 from pytube import YouTube
 from sys import argv
 
@@ -7,5 +9,8 @@ yt = YouTube(link)
 print("Title: ", yt.title)
 print("Views: ", yt.views)
 yd = yt.streams.get_audio_only()
+
+# For Video download use this
+# yd = yt.streams.get_highest_resolution()
 
 yd.download('/Users/sohan/Music/YT music')
